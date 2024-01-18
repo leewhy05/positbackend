cc48dbca19f1af926f43caa91efd2d6f529bab0d
leewhy05 <leewhy1105@gmail.com>
2024-01-18 07:47:01 -0700

    first message

--- routes/postRoute.js
+++ routes/postRoute.js
@@ -24,7 +24,7 @@ const auth = require("../middleware/auth");
 
 router.post("/posts", auth, createPost);
 router.get("/allposts", getAllDescription);
-router.get("/getpost/:postId", singlePost);
+router.get("/getpost/:postId", auth, singlePost);
 router.get("/userpost/:userId", auth, getUserPosts);
 router.delete("/delete/:postId", auth, deleteUserPost);
 router.patch("/update/:postId", auth, updateUserPost);
